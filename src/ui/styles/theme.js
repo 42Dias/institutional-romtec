17f55f9ea11b1414e185e764a4dfad90505ca8ab
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-18 11:41:45 -0300

    adding config

---
+++ src/ui/styles/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    white: '#FAFAFA',
+    black: '#252332',
+    darkBlack: '#1B1925',
+    yellow: '#FECE51',
+  },
+  font: {
+    family: 'Poppins',
+    weigths: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+}
