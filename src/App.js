a4bcc6fd0134bef7f8a50cd7c53b7fd548e35388
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-18 13:36:38 -0300

    adding style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>hello world</h1>
+      <header>
+        <ul>
+          <li>lorem</li>
+          <li>lorem</li>
+          <li>lorem</li>
+          <li>lorem</li>
+        </ul>
+      </header>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+      <p>
+        Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.
+        Suspendisse libero magna, lobortis ac neque non, porttitor ornare quam.
+        Ut non pretium leo.
+      </p>
+      <a href='#'>Saiba mais</a>
+      <footer>Lorem</footer>
     </>
   )
 }
