6c077a906f57c76d649940a9477d58a6188a4dc4
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-19 09:57:18 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import { GlobalStyle } from './ui'
+import logo from './assets/logo.svg'
+import header from './assets/header.png'
+import * as S from './App.styled'
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <header>
-        <ul>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-        </ul>
+        <img src={logo} alt='logo' />
+        <nav>
+          <ul>
+            <S.List>
+              <a href=''>lorem</a>
+            </S.List>
+            <S.List>
+              <a href=''>lorem</a>
+            </S.List>
+            <S.List>
+              <a href=''>lorem</a>
+            </S.List>
+            <S.List>
+              <a href=''>lorem</a>
+            </S.List>
+          </ul>
+        </nav>
       </header>
-      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-      <p>
-        Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.
-        Suspendisse libero magna, lobortis ac neque non, porttitor ornare quam.
-        Ut non pretium leo.
-      </p>
-      <a href='#'>Saiba mais</a>
+      <div>
+        <img src={header} alt='logo' />
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+        <p>
+          Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.
+          Suspendisse libero magna, lobortis ac neque non, porttitor ornare
+          quam. Ut non pretium leo.
+        </p>
+        <a href='#'>Saiba mais</a>
+      </div>
       <footer>Lorem</footer>
     </>
   )
---
+++ src/App.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/macro'
+
+export const List = styled.li`
+  display: inline;
+`
